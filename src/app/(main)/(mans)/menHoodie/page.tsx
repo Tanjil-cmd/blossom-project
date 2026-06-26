059f61e484efa6dcd141257809d2f4d769524b67
Tanjil-cmd <tanjil.bd44@gmail.com>
2026-06-26 18:23:14 +0600

    fix resend

--- src/app/(main)/(mans)/menHoodie/page.tsx
+++ src/app/(main)/(mans)/menHoodie/page.tsx
@@ -1,48 +1,48 @@
 import ShowImage from '@/components/ShowImage';
 import React from 'react';
-// import menHoodie_1 from '@/assets/Mens/hoodie/rsz_hoodie-1.png'
-// import menHoodie_2 from '@/assets/Mens/hoodie/rsz_hoodie-2.png'
-// import menHoodie_3 from '@/assets/Mens/hoodie/hoodie-3.jpg';
-// import menHoodie_4 from '@/assets/Mens/hoodie/hoodie-3.png';
-// import menHoodie_5 from '@/assets/Mens/hoodie/hoodie-4.jpg';
-// import menHoodie_6 from '@/assets/Mens/hoodie/rsz_hoodie-4.png';
-// import menHoodie_7 from '@/assets/Mens/hoodie/philipp-lansing-REOoETloFqE-unsplash.jpg';
+import menHoodie_1 from '@/assets/Mens/hoodie/rsz_hoodie-1.png'
+import menHoodie_2 from '@/assets/Mens/hoodie/rsz_hoodie-2.png'
+import menHoodie_3 from '@/assets/Mens/hoodie/hoodie-3.jpg';
+import menHoodie_4 from '@/assets/Mens/hoodie/hoodie-3.png';
+import menHoodie_5 from '@/assets/Mens/hoodie/hoodie-4.jpg';
+import menHoodie_6 from '@/assets/Mens/hoodie/rsz_hoodie-4.png';
+import menHoodie_7 from '@/assets/Mens/hoodie/philipp-lansing-REOoETloFqE-unsplash.jpg';
 
 const MenHoodie = () => {
-    // const photos = [
-    //     {
-    //         id: 1,
-    //         photo: menHoodie_1
-    //     },
-    //     {
-    //         id: 2,
-    //         photo: menHoodie_2
-    //     },
-    //     {
-    //         id: 3,
-    //         photo: menHoodie_3
-    //     },
-    //     {
-    //         id: 4,
-    //         photo: menHoodie_4
-    //     },
-    //     {
-    //         id: 5,
-    //         photo: menHoodie_5
-    //     },
-    //     {
-    //         id: 6,
-    //         photo: menHoodie_6
-    //     },
-    //     {
-    //         id: 7,
-    //         photo: menHoodie_7
-    //     }
-    // ]
+    const photos = [
+        {
+            id: 1,
+            photo: menHoodie_1
+        },
+        {
+            id: 2,
+            photo: menHoodie_2
+        },
+        {
+            id: 3,
+            photo: menHoodie_3
+        },
+        {
+            id: 4,
+            photo: menHoodie_4
+        },
+        {
+            id: 5,
+            photo: menHoodie_5
+        },
+        {
+            id: 6,
+            photo: menHoodie_6
+        },
+        {
+            id: 7,
+            photo: menHoodie_7
+        }
+    ]
     return (
         <div>
             <h3 className='text-3xl font-bold text-center'>Men Hoodie</h3>
-            {/* <ShowImage photos={photos} /> */}
+            <ShowImage photos={photos} />
         </div>
     );
 };
--- src/components/ProfessionalDress.tsx
+++ src/components/ProfessionalDress.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
-// import hoodie_1 from '@/assets/Mens/hoodie/rsz_hoodie-1.png';
-// import hoodie_2 from '@/assets/Mens/hoodie/rsz_hoodie-2.png';
-// import hoodie_3 from '@/assets/Mens/hoodie/hoodie-3.png';
-// import hoodie_4 from '@/assets/Mens/hoodie/hoodie-4.jpg';
+import hoodie_1 from '@/assets/Mens/hoodie/rsz_hoodie-1.png';
+import hoodie_2 from '@/assets/Mens/hoodie/rsz_hoodie-2.png';
+import hoodie_3 from '@/assets/Mens/hoodie/hoodie-3.png';
+import hoodie_4 from '@/assets/Mens/hoodie/hoodie-4.jpg';
 
 const ProfessionalDress = () => {
-    // const hoodies = [hoodie_1, hoodie_2, hoodie_3, hoodie_4];
+    const hoodies = [hoodie_1, hoodie_2, hoodie_3, hoodie_4];
     return (
         <div className='max-w-[1200px] mx-auto px-3 py-10'>
             <h2 className='md:text-3xl text-2xl font-bold'>Timeless elegance and sophistication, crafted for every <br />
                 professional and special occasion.</h2>
 
-                {/* <div className='grid md:grid-cols-4 gap-5 mt-4'>
+                <div className='grid md:grid-cols-4 gap-5 mt-4'>
                     {
                         hoodies.map((data, index) => 
                         <div key={index}>
@@ -22,7 +22,7 @@ const ProfessionalDress = () => {
                         </div>
                         )
                     }
-                </div> */}
+                </div>
         </div>
     );
 };
